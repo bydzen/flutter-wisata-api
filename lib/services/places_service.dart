dc7904aa8e0771636157e1c0d7eed6e6da74637a
bydzen <svnteentthones@gmail.com>
2022-06-25 02:05:10 +0700

    first commit

---
+++ lib/services/places_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_api/models/places_model.dart';
+import 'package:http/http.dart' as http;
+
+class Places {
+  Future<List<Destination>?> getDestination() async {
+    var client = http.Client();
+    var uri = Uri.parse('https://locavel.herokuapp.com/wisata');
+
+    var response = await client.get(uri);
+
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return destinationFromJson(json);
+    }
+  }
+}
